1b83440644bf47a2d468ee11dacc7343dd67d0fb
Andre <andrebtoe@hotmail.com>
2021-04-22 12:06:27 -0300

    Add limit test

---
+++ src/FluentSqlBuilder.Test.Unit/SelectLimitTest.cs
@@ -0,0 +1,33 @@
+﻿using FluentSqlBuilder.DataModel;
+using SqlBuilderFluent;
+using SqlBuilderFluent.Types;
+using Xunit;
+
+namespace FluentSqlBuilder.Test.Unit
+{
+    public class SelectLimitTest
+    {
+        private static SqlAdapterType _typeDefault = SqlAdapterType.SqlServer2019;
+        private static SqlBuilderFormatting _formattingDefault = SqlBuilderFormatting.Indented;
+
+        [Fact]
+        public void Test_Select_Limit()
+        {
+            // Arrange
+            var tableName = "order";
+            var limit = 10;
+            var sqlBuilderWithoutAlias = new FluentSqlBuilder<OrderDataModel>(_typeDefault, _formattingDefault)
+                                             .Limit(limit);
+
+            // Act
+            var sqlSelect = sqlBuilderWithoutAlias.ToString();
+
+            // Assert
+            Assert.True(sqlSelect.Contains($"SELECT TOP({limit})"), $"FROM invalid");
+            Assert.True(sqlSelect.Contains($"FROM [checkout].[{tableName}]"), $"FROM invalid");
+            Assert.True(sqlSelect.Contains($"[{tableName}].[Id]"), $"Column not found");
+            Assert.True(sqlSelect.Contains($"[{tableName}].[customer_id]"), $"Column not found");
+            Assert.True(sqlSelect.Contains($"[{tableName}].[Status]"), $"Column not found");
+        }
+    }
+}
